ab56fc7cdf6f34b621d25f3afe08c6ae6d43e68c
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-23 11:12:05 +0900

    Title: 합과 평균의 차, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251023/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let total = a + b + c;
+
+console.log(total);
+console.log(total / 3);
+
+let average = total / 3;
+
+console.log(total - average);
+
