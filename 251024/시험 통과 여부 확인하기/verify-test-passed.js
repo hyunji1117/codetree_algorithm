023461c540494aad690061431c6215f7af56ce16
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-24 10:29:35 +0900

    Title: 시험 통과 여부 확인하기, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251024/시험 통과 여부 확인하기/verify-test-passed.js
+++ 251024/시험 통과 여부 확인하기/verify-test-passed.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
-if (input > 80) {
+let N = Number(input);
+
+if (N > 80) {
     console.log("pass");
 } else {
-    x = 80 - input;
+    x = 80 - N;
     console.log(`${x} more score`);
 }
