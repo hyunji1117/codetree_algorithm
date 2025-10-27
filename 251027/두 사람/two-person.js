d0b6076c325e5b8840789140b040cc9c606338d8
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-27 10:51:48 +0900

    Title: 두 사람, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251027/두 사람/two-person.js
+++ 251027/두 사람/two-person.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let [age_A, sex_A] = input[0].split(" ").map(Number);
-let [age_B, sex_B] = input[1].split(" ").map(Number);
+let [age_A, sex_A] = input[0].split(" ");
+let [age_B, sex_B] = input[1].split(" ");
+age_A = Number(age_A);
+age_B = Number(age_B);
 
-if (age_A >= 19 || age_B >= 19 && (sex_A === "M" && sex_B === "M")) {    
+if ((age_A >= 19 && sex_A === "M") || (age_B >= 19 && sex_B === "M")) {    
     console.log("1");
 }
 else {
