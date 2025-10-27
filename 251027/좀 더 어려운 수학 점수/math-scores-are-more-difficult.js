cdcb86b7098975234186c5949034020745997eec
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-27 10:35:15 +0900

    Title: 좀 더 어려운 수학 점수, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251027/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 251027/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 let [math_A, english_A] = input[0].split(" ").map(Number);
 let [math_B, english_B] = input[1].split(" ").map(Number);
 
-if ((math_A > english_A && math_B > english_B && (math_A > math_B)) || (math_A === math_B && (english_A > english_B))) {
+if (math_A > math_B || math_A === math_B && english_A > english_B) {
     console.log("A");
 }
 else {
