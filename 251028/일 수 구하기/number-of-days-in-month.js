3676fd150ff859beee79d36d4ced0f0625b13c5b
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-28 10:57:50 +0900

    Title: 일 수 구하기, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251028/일 수 구하기/number-of-days-in-month.js
+++ 251028/일 수 구하기/number-of-days-in-month.js
@@ -1,8 +1,19 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
-if (n === 1 || n > 2 && n <= 12) {
-    console.log("31");
-} else if (n === 2) {
-    console.log("28");
+if (n >= 1 && n < 7) {
+    if (n % 2 === 1) {
+        console.log("31");
+    } else if (n === 2) {
+        console.log("28");
+    } else {
+        console.log("30");
+    }
+} else if (n >= 8 && n <= 12 ) {
+    if (n % 2 === 0) {
+        console.log("31")
+    } else {
+        console.log("30");
+    }
 }
+
