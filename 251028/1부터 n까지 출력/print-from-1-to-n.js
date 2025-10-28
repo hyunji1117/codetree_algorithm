714d50bb159b7690aeec7511439f9872a1057fe7
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-28 16:32:15 +0900

    Title: 1부터 n까지 출력, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251028/1부터 n까지 출력/print-from-1-to-n.js
+++ 251028/1부터 n까지 출력/print-from-1-to-n.js
@@ -1,4 +1,10 @@
 const fs = require("fs");
 let N = Number(fs.readFileSync(0).toString().trim());
 
-console.log(N);
+let result = "";
+
+for (i = 1; i <= N; i++) {
+    result += (i + " ");
+}
+
+console.log(result);
