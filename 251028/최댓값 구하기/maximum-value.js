2e2fc12cf2de2f0e4c3602417106e2620b911fb6
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-28 11:12:03 +0900

    Title: 최댓값 구하기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 251028/최댓값 구하기/maximum-value.js
+++ 251028/최댓값 구하기/maximum-value.js
@@ -5,10 +5,12 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 let c = Number(input[2]);
 
-if (a > b && a > c) {
-    console.log(a);
-} else if (b > a && b > c) {
-    console.log(b)
-} else {
-    console.log(c);
+if ((a >= -100 && a <= 100) && (b >= -100 && b <= 100) && (c >= -100 && c <= 100)) {
+    if (a >= b && a >= c) {
+        console.log(a);
+    } else if (b >= a && b >= c) {
+        console.log(b)
+    } else {
+        console.log(c);
+    }
 }
