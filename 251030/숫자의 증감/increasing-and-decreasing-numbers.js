2c8919ff22294bba11fe56d7e71296a921fc0878
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-30 13:20:18 +0900

    Title: 숫자의 증감, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251030/숫자의 증감/increasing-and-decreasing-numbers.js
@@ -0,0 +1,25 @@
+// const fs = require("fs");
+// let N = fs.readFileSync(0).toString().trim();
+
+// const fs = require("fs");
+// let N = Number(fs.readFileSync(0).toString().trim());
+
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let C = input[0];
+let N = Number(input[1]);
+let result1 = "";
+let result2 = "";
+
+if (C === "A"){
+    for (let i = 1; i <= N; i++) {
+        result1 += (i + " ");
+    }
+    console.log(result1);
+} else if (C === "D") {
+    for (let i = N; i >= 1; i--) {
+        result2 += (i + " ");
+    }
+    console.log(result2);
+}
