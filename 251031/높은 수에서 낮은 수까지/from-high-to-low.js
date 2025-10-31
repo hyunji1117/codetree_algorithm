ded120e7a32612d8385671ceb02c8d8c5da64397
김현지 (Evelyn.Kim) <151576407+hyunji1117@users.noreply.github.com>
2025-10-31 09:46:51 +0900

    Title: 높은 수에서 낮은 수까지, Time: 60ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251031/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
+
+let A = Number(input[0]);
+let B = Number(input[1]);
+
+let result = "";
+
+if (A > B) {
+    for (let i = A; i >= B; i--) {
+        result += (i + " ");
+    }
+    console.log(result);
+} else if (B > A) { 
+    for (let i = B; i >= A; i--) {
+        result += (i + " ");
+    }
+    console.log(result);
+}
